f5a50526614b40738cbabda4c4e3c1df871fc97f
AlanAvila97 <fidelavila21@gmail.com>
2024-01-31 15:47:33 -0600

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,25 @@
 <template>
     <main id="wrapper" class="bg-home index">
-        
+        <section class="pt-md-70 section-slider-head">
+            <div class="container-slider-head">
+                <div data-aos="fade-down">
+                    <div>
+                        <div v-if="$viewport.isLessThan('tablet')">Should render only on mobile</div>
+                        <div v-else>Current breakpoint: {{ $viewport.breakpoint }}</div>
+                    </div>
+                </div>
+            </div>
+        </section>
     </main>
 </template>
-
 <script setup>
-</script>
+    import { useNuxtApp } from '#app'
+    const { $viewport } = useNuxtApp()
 
+    watch($viewport.breakpoint, (newBreakpoint, oldBreakpoint) => {
+        console.log('Breakpoint updated:', oldBreakpoint, '->', newBreakpoint)
+    })
+</script>
 <style lang="scss" scoped>
 
 </style>
