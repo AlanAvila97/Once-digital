68abf9052cc3a7c1b8da7705f065e80b0ec19ad6
AlanAvila97 <fidelavila21@gmail.com>
2024-01-31 15:47:29 -0600

    Configuraciones de modulos

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -60,9 +60,63 @@ export default defineNuxtConfig({
   ],
   ssr: true,
   modules: [ 
+    'nuxt-aos',
     'nuxt-icon',
+    'nuxt-viewport',
     'nuxt-typed-router',
-    'nuxt-link-checker'
+    'nuxt-link-checker',
+    'nuxt-payload-analyzer',
+    '@nuxthq/studio',
   ],
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  aos: {
+    // Global settings:
+    disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
+    startEvent: 'DOMContentLoaded', // name of the event dispatched on the document, that AOS should initialize on
+    initClassName: 'aos-init', // class applied after initialization
+    animatedClassName: 'aos-animate', // class applied on animation
+    useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
+    disableMutationObserver: false, // disables automatic mutations' detections (advanced)
+    debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
+    throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
+    
+
+    // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+    offset: 120, // offset (in px) from the original trigger point
+    delay: 0, // values from 0 to 3000, with step 50ms
+    duration: 400, // values from 0 to 3000, with step 50ms
+    easing: 'ease', // default easing for AOS animations
+    once: false, // whether animation should happen only once - while scrolling down
+    mirror: false, // whether elements should animate out while scrolling past them
+    anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
+  },
+  viewport: {
+    breakpoints: {
+      desktop: 1024,
+      desktopMedium: 1280,
+      desktopWide: 1600,
+
+      mobile: 320,
+      mobileMedium: 375,
+      mobileWide: 425,
+
+      tablet: 768,
+    },
+
+    cookie: {
+      expires: 365, // 365 days
+      name: 'viewport',
+      path: '/',
+      sameSite: 'Strict',
+      secure: true,
+    },
+
+    defaultBreakpoints: {
+      desktop: 'desktop',
+      mobile: 'mobile',
+      tablet: 'tablet',
+    },
+
+    fallbackBreakpoint: 'desktop',
+  },
 })
